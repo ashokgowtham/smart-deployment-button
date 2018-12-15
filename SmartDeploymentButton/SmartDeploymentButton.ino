9c45bf3157bfe31aeba35d052a25e83340edecc7
Ashok Gowtham M <ashokgowtham123@gmail.com>
2018-12-15 10:48:49 +0530

    Initial Commit Basic Wifi connection

---
+++ SmartDeploymentButton/SmartDeploymentButton.ino
@@ -0,0 +1,45 @@
+#include "ESP8266WiFi.h"
+
+const char* wifi_ssid = FILL_SSID_HERE;
+const char* wifi_password = FILL_PASSWORD_HERE;
+
+WiFiClient espClient;
+
+
+void setupWifi() {
+  delay(100);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(wifi_ssid);
+
+  WiFi.begin(wifi_ssid, wifi_password);
+
+  int tries = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    tries++;
+    if (tries>20) {
+      Serial.print("Connection failed. Rebooting...");
+      ESP.restart();
+    }
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  delay( 3000 ); // power-up safety delay
+  Serial.begin(115200);
+  setupWifi();
+}
+
+
+void loop()
+{
+    
+}
